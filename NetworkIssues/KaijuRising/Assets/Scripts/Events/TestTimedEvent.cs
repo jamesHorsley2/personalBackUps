9df3980f3b2ce95a7dd18d57c080b8dabe7959a6
James Horsley <decardcain37@gmail.com>
2015-10-08 23:57:03 +1100

    working on spawning aoe over network

---
+++ NetworkIssues/KaijuRising/Assets/Scripts/Events/TestTimedEvent.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestTimedEvent : TimedEvent 
+{
+	public void Start()
+	{
+		activateEvent();
+	}
+
+	protected override void runEventEffects()
+	{
+		print("Event is active");
+	}
+
+	protected override void finishEvent()
+	{
+		print("Event has ended");
+	}
+
+	protected override void activateEvent()
+	{
+		base.activateEvent();
+		print("event activated");
+	}
+}
