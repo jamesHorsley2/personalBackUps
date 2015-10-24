0e4a732483159a73f014e7bb823b4a6278ef438c
James Horsley <decardcain37@gmail.com>
2015-10-24 15:16:56 +1100

    Finished Sorter

---
+++ KaijuRising/Assets/newBuildings/TextureSwapper.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TextureSwapper : MonoBehaviour {
+	
+	public int amountInList;
+	public List<List<Material>> ObjectCatagories;
+	//public GameObject[][] ObjectCatagories;
+	public string[] nameArray;
+	public List<GameObject> storedMaterials;
+
+	private void Start()
+	{	
+		storeValues();
+		searchBuilding();
+		print (ObjectCatagories[1][5]);
+	}
+
+	private void storeValues()
+	{
+		ObjectCatagories = new List<List<Material>>(amountInList);
+		//creates the different sections to sort through the lists with
+		for(int q =0; q < amountInList; q++)
+		{
+			ObjectCatagories.Add(new List<Material>());
+		}
+	}
+
+	private void searchBuilding()
+	{
+		Renderer[] localMaterial = GetComponentsInChildren<Renderer>() as Renderer[];
+
+		for(int z=0; z < amountInList; z++)
+		{
+			for(int i=0; i<localMaterial.Length; i++)
+			{
+				if(localMaterial[i].gameObject.name.Contains(nameArray[z]))
+				{
+	
+					ObjectCatagories[z].Add(localMaterial[i].material);
+					print (ObjectCatagories[z]);
+					//ObjectCatagories[z][i] = localMaterial[i].gameObject;
+					//storedMaterials.Add (localMaterial[i].gameObject);
+	
+				}
+			}
+		}
+	}
+
+}
---
+++ KaijuRising/Assets/newBuildings/tankTester.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tankTester : MonoBehaviour {
+
+	public GameObject objectToTank;
+	public int offset;
+	public int amountToSpawn;
+	public int row;
+
+	private void Start()
+	{
+		for(int i = 0; i < amountToSpawn; i++)
+		{
+			for(int z = 0; z < row; z++) 
+			{
+				Instantiate(objectToTank,spawnPos(i,z),Quaternion.identity);
+			}
+		}
+	}
+
+
+	private Vector3 spawnPos(float x, float z)
+	{
+		return new Vector3(x * offset,0f,z * offset);
+	}
+}
