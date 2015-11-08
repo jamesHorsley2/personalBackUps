e6cb478304acbb15d06ea31a52669d1352ff2831
James Horsley <decardcain37@gmail.com>
2015-11-08 20:37:00 +1100

    Working on GameBoy assembly + other puzzle stuff

--- Puzzel/Assets/Scripts/Movement/GridMovement.cs
+++ Puzzel/Assets/Scripts/Movement/GridMovement.cs
@@ -3,46 +3,50 @@ using System.Collections;
 
 public class GridMovement : MonoBehaviour {
 
-	public string layerName;
 	private RaycastHit checkAllowedMovement;
-	private float maxDistance;
 	private int layermask;
 	private bool allowMovemnt = false;
+	private bool[] canMove = new bool[4], controls = new bool[4];
+	//private Rigidbody rb;
+
+
+	public float maxDistance, speed;
+	public string layerName;
 
 	private void Start()
 	{
 		layermask = LayerMask.NameToLayer(layerName);
+	//	rb = GetComponent<Rigidbody>();
+		checkCanMove();
 	}
+
 
 	private void Update()
 	{
+		controls[0] = Input.GetKey(KeyCode.UpArrow);
+		controls[1] = Input.GetKey(KeyCode.LeftArrow);
+		controls[2] = Input.GetKey(KeyCode.DownArrow);
+		controls[3] = Input.GetKey(KeyCode.RightArrow);
 
-	}
 
-	private void FixedUpdate()
-	{
-		if(Input.GetKeyDown(KeyCode.Space))
+		if(controls[0] && canMove[0])
 		{
-			checkCanMove();
+			movement(transform.forward, speed);
+		}
+		else if(controls[1] && canMove[1])
+		{
+			movement(-transform.right, speed);
+		}
+		else if(controls[2] && canMove[2])
+		{
+			movement(-transform.forward, speed);
+		}
+		else if(controls[3] && canMove[3])
+		{
+			movement(transform.right, speed);	
 		}
 	}
 	
-	private void checkDirection()
-	{
-
-	}
-
-	private void checkCanMove()
-	{
-		for(int i=0; i < 4; i++)
-		{
-			if(Physics.Raycast(transform.position + changeCheckDirection(i), -transform.up,out checkAllowedMovement, layermask))
-			{
-				Debug.DrawLine(transform.position + changeCheckDirection(i), new Vector3 (0,0,0),Color.red,5f);
-				print("There is ground below");
-			}
-		}
-	}
 
 	private Vector3 changeCheckDirection(int dir)
 	{
@@ -51,18 +55,45 @@ public class GridMovement : MonoBehaviour {
 		{
 			case 0:
 				savedDir = transform.forward;
+				//savedDir = Vector3.forward;
 				break;
 			case 1:
 				savedDir = -transform.right;
+				//savedDir = Vector3.left;
 				break;
 			case 2:
 				savedDir = -transform.forward;
+				//savedDir = Vector3.back;
 				break;
 			case 3:
 				savedDir = transform.right;
+				//savedDir = Vector3.right;
 				break;
 		}
 		return savedDir;
 	}
 
+	private void checkCanMove()
+	{
+		for(int i=0; i < 4; i++)
+		{
+			if(Physics.Raycast(transform.position + changeCheckDirection(i), -transform.up,out checkAllowedMovement, layermask))
+			{
+				canMove[i] = true;
+				//Debug.DrawLine(transform.position + changeCheckDirection(i), new Vector3 (0,0,0),Color.red,5f);
+			}
+			else
+			{
+				canMove[i] = false;
+			}
+		}
+	}
+
+	private void movement(Vector3 direction, float speed)
+	{
+		//rb.velocity = direction * speed;
+		//rb.AddForce(direction * speed);
+		transform.Translate(direction * Time.deltaTime * speed);
+		checkCanMove();
+	}
 }
---
+++ Puzzel/Assets/Scripts/Movement/path.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class path : MonoBehaviour {
+
+	public List<Vector3> openList, closedList;
+	private GameObject[] xArray, zArray;
+	public GameObject spawnPrefab, save;
+	private float offSet = 10f; 
+
+	private void Start()
+	{
+		xArray = new GameObject[7];
+		zArray = new GameObject[6];
+		makeGrid();
+
+	}
+	
+
+	private void makeGrid()
+	{
+		for(int x=0; x < xArray.Length ;x++)
+		{
+			for(int z=0; z < zArray.Length; z++)
+			{
+				save = Instantiate(spawnPrefab,new Vector3(x * offSet,0,z * offSet),Quaternion.identity) as GameObject;
+				//print(save.GetComponent<Collider>().name);
+			}
+		}
+	}
+
+	private void OnCollisonEnter(Collision col)
+	{
+		if(col.gameObject)
+		{
+			print("getCol");
+		}
+	}
+}
